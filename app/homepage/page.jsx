e67c6753e11efce33a221cbb3045dfcfcaca030b
AmmyAstro <amrender@dhwaniastro.com>
2025-03-05 14:54:43 +0530

    final done webhome page

--- app/homepage/page.jsx
+++ app/homepage/page.jsx
@@ -1,49 +1,8 @@
-import Aboutdirect from "@/components/Aboutdirect";
-import AboutUs from "@/components/AboutUs";
-import Astrocard from "@/components/Astrocard";
-import Astroserv from "@/components/Astroserv";
-import Astrostore from "@/components/Astrostore";
-import Banner from "@/components/Banner";
-import Blogsection from "@/components/Blogsection";
-import Chathoro from "@/components/Chathoro";
-import Consulque from "@/components/Consulque";
-import Credent from "@/components/Credent";
-import Downapp from "@/components/Downapp";
-import FAQue from "@/components/FAQue";
-import Footerlinks from "@/components/Footerlinks";
-import Header from "@/components/Header";
-import NavBar from "@/components/NavBar";
-import Pujahome from "@/components/Pujahom";
-import Pujaupcom from "@/components/Pujaupcom";
-import RemeCalc from "@/components/Remecalc";
-import Testimon from "@/components/Testimon";
+import Mainhomecom from "@/components/Mainhomecom";
+
 
 export default function Home() {
   return (
-    <div className="">
-      <div className="head_nav_top_all fixed top-0 left-0 z-999">
-        <Header />
-        <NavBar />
-      </div>
-     <div className="main_body-content flex flex-col  ">
-     <Banner />
-     <Chathoro />
-     <Astrocard/>  
-     <Astroserv/>
-     <RemeCalc/>
-     <Pujahome/>
-     <Pujaupcom/>
-     <Downapp/>
-     <Astrostore/>
-     <Consulque/>  
-     <Aboutdirect/>
-     <Credent />
-     <Blogsection/>
-     <FAQue/>
-     <Testimon/>
-     <AboutUs />
-     <Footerlinks/>
-     </div>
-    </div>
+  <Mainhomecom/>
   );
 }
---
+++ components/Mainhomecom.jsx
@@ -0,0 +1,56 @@
+"use client";
+import Header from "./Header";
+import NavBar from "./NavBar";
+import Banner from "./Banner";
+import Chathoro from "./Chathoro";
+import Astrocard from "./Astrocard";
+import Astroserv from "./Astroserv";
+import RemeCalc from "./Remecalc";
+import Pujahome from "./Pujahom";
+import Pujaupcom from "./Pujaupcom";
+import Downapp from "./Downapp";
+import Astrostore from "./Astrostore";
+import Consulque from "./Consulque";
+import Aboutdirect from "./Aboutdirect";
+import Credent from "./Credent";
+import Blogsection from "./Blogsection";
+import FAQue from "./FAQue";
+import Testimon from "./Testimon";
+import AboutUs from "./AboutUs";
+import Footerlinks from "./Footerlinks";
+
+
+
+
+export default function Mainhomecom() {
+   
+
+  return (
+    <div className="">
+    <div className="head_nav_top_all fixed top-0 left-0 z-999">
+      <Header />
+      <NavBar />
+    </div>
+   <div className="main_body-content flex flex-col  ">
+   <Banner />
+   <Chathoro />
+   <Astrocard/>  
+   <Astroserv/>
+   <RemeCalc/>
+   <Pujahome/>
+   <Pujaupcom/>
+   <Downapp/>
+   <Astrostore/>
+   <Consulque/>  
+   <Aboutdirect/>
+   <Credent />
+   <Blogsection/>
+   <FAQue/>
+   <Testimon/>
+   <AboutUs />
+   <Footerlinks/>
+   </div>
+  </div>
+     
+      );
+    }
